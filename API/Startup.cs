f89a7adccc937ef5b8ca6852392cce45febdec44
Carrie Schmid <carriepederson7@gmail.com>
2020-03-11 22:09:01 -0500

    Add Attendee.cs

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,6 +38,7 @@ namespace API {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices (IServiceCollection services) {
             services.AddDbContext<DataContext> (opt => {
+                opt.UseLazyLoadingProxies ();
                 opt.UseSqlite (Configuration.GetConnectionString ("DefaultConnection"));
             });
             services.AddCors (opt => {
--- Domain/UserActivity.cs
+++ Domain/UserActivity.cs
@@ -3,9 +3,9 @@ using System;
 namespace Domain {
     public class UserActivity {
         public string AppUserId { get; set; }
-        public AppUser AppUser { get; set; }
+        public virtual AppUser AppUser { get; set; }
         public Guid ActivityId { get; set; }
-        public Activity Activity { get; set; }
+        public virtual Activity Activity { get; set; }
 
         public DateTime DateJoined { get; set; }
 
